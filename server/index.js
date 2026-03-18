8731c83fadb53a63743668dfe80e53537203d39b
kratisha434 <hkratisha@gmail.com>
2026-03-18 13:51:58 +0530

    added root route to fix cannot GET error

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(json());
+app.get("/", (req, res) => {
+  res.send("ArtRoom API is running...");
+});
 
 app.use('/api/journal', journalRoutes);
 app.use('/api/poetry', poetryRoutes);
